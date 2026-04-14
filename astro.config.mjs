18ee17fa1788dafb58db725c2f12739c59eb02f5
shtirlizc <shtirlizc@gmail.com>
2026-04-14 10:43:26 +0500

    update data, remove extra blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
-import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://shtirlizc.github.io",
   base: "/portfolio",
-  integrations: [react()],
 });
