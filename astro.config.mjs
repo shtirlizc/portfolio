5bec0c4b0060b761d077a15787686eeca1c27e7a
shtirlizc <shtirlizc@gmail.com>
2026-04-14 17:33:14 +0500

    Revert "fix config"
    This reverts commit 88da5248ba54d68ecf79e29857883570c495f99d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shtirlizc.ru"
+  site: "https://shtirlizc.github.io",
+  base: "/portfolio",
 });
